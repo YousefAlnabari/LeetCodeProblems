4797bb40bf6471541383d6d2940b1e338e649108
Yousef Alnabari <157176097+YousefAlnabari@users.noreply.github.com>
2024-04-18 21:44:14 +0300

    Create 7. Reverse Integer.java

---
+++ Java/7. Reverse Integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        boolean isNegative = x < 0;
+        x = Math.abs(x);
+        long reversed = 0;
+        while(x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+        }
+        if(reversed > Integer.MAX_VALUE)
+            return 0;
+        return isNegative ? -(int) reversed : (int) reversed;
+    }
+}
